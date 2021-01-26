4cefd636348335169cdce28bde4c75ff009494cd
Wenzhi Li <lwz964743608@gmail.com>
2021-01-26 11:03:11 +0800

    modify entity class and Application class

--- SpringRestful/src/main/java/com/example/sr/Application.java
+++ SpringRestful/src/main/java/com/example/sr/Application.java
@@ -1,8 +1,14 @@
 package com.example.sr;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author dell
+ * @des 运行启动springboot应用
+ */
+@MapperScan("com.example.sr.mapper") // 指定mapper所在位置 扫描mapper 
 @SpringBootApplication
 public class Application {
 
--- SpringRestful/src/main/java/com/example/sr/entity/AccEntity.java
+++ SpringRestful/src/main/java/com/example/sr/entity/AccEntity.java
@@ -2,4 +2,32 @@ package com.example.sr.entity;
 
 public class AccEntity {
 
+	private int id;
+	private String username;
+	private String password;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 }
